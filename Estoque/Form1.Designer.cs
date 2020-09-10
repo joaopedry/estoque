aee18ddc4db9930e801433b0245ca450b8ceeae2
joaopedry <36898182+joaopedry@users.noreply.github.com>
2020-09-10 13:45:33 -0300

    Add all local files

---
+++ Estoque/Form1.Designer.cs
@@ -0,0 +1,141 @@
+﻿namespace Estoque
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lbEstoque = new System.Windows.Forms.Label();
+            this.btAtualizar = new System.Windows.Forms.Button();
+            this.lbQtdEstoque = new System.Windows.Forms.Label();
+            this.grvProduto = new System.Windows.Forms.DataGridView();
+            this.grvVenda = new System.Windows.Forms.DataGridView();
+            this.lbProduto = new System.Windows.Forms.Label();
+            this.lbVenda = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.grvProduto)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.grvVenda)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // lbEstoque
+            // 
+            this.lbEstoque.AutoSize = true;
+            this.lbEstoque.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbEstoque.Location = new System.Drawing.Point(15, 256);
+            this.lbEstoque.Name = "lbEstoque";
+            this.lbEstoque.Size = new System.Drawing.Size(214, 25);
+            this.lbEstoque.TabIndex = 0;
+            this.lbEstoque.Text = "Quantidade Estoque:";
+            // 
+            // btAtualizar
+            // 
+            this.btAtualizar.Location = new System.Drawing.Point(444, 244);
+            this.btAtualizar.Name = "btAtualizar";
+            this.btAtualizar.Size = new System.Drawing.Size(108, 37);
+            this.btAtualizar.TabIndex = 1;
+            this.btAtualizar.Text = "Atualizar";
+            this.btAtualizar.UseVisualStyleBackColor = true;
+            this.btAtualizar.Click += new System.EventHandler(this.btAtualizar_Click);
+            // 
+            // lbQtdEstoque
+            // 
+            this.lbQtdEstoque.AutoSize = true;
+            this.lbQtdEstoque.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbQtdEstoque.Location = new System.Drawing.Point(226, 257);
+            this.lbQtdEstoque.Name = "lbQtdEstoque";
+            this.lbQtdEstoque.Size = new System.Drawing.Size(22, 24);
+            this.lbQtdEstoque.TabIndex = 2;
+            this.lbQtdEstoque.Text = "--";
+            // 
+            // grvProduto
+            // 
+            this.grvProduto.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.grvProduto.Location = new System.Drawing.Point(20, 66);
+            this.grvProduto.Name = "grvProduto";
+            this.grvProduto.Size = new System.Drawing.Size(240, 168);
+            this.grvProduto.TabIndex = 3;
+            // 
+            // grvVenda
+            // 
+            this.grvVenda.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.grvVenda.Location = new System.Drawing.Point(312, 66);
+            this.grvVenda.Name = "grvVenda";
+            this.grvVenda.Size = new System.Drawing.Size(240, 168);
+            this.grvVenda.TabIndex = 4;
+            // 
+            // lbProduto
+            // 
+            this.lbProduto.AutoSize = true;
+            this.lbProduto.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbProduto.Location = new System.Drawing.Point(15, 38);
+            this.lbProduto.Name = "lbProduto";
+            this.lbProduto.Size = new System.Drawing.Size(98, 25);
+            this.lbProduto.TabIndex = 5;
+            this.lbProduto.Text = "Produtos";
+            // 
+            // lbVenda
+            // 
+            this.lbVenda.AutoSize = true;
+            this.lbVenda.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbVenda.Location = new System.Drawing.Point(307, 38);
+            this.lbVenda.Name = "lbVenda";
+            this.lbVenda.Size = new System.Drawing.Size(85, 25);
+            this.lbVenda.TabIndex = 6;
+            this.lbVenda.Text = "Vendas";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(573, 290);
+            this.Controls.Add(this.lbVenda);
+            this.Controls.Add(this.lbProduto);
+            this.Controls.Add(this.grvVenda);
+            this.Controls.Add(this.grvProduto);
+            this.Controls.Add(this.lbQtdEstoque);
+            this.Controls.Add(this.btAtualizar);
+            this.Controls.Add(this.lbEstoque);
+            this.Name = "Form1";
+            this.Text = "Sistema de Estoque";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.grvProduto)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.grvVenda)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lbEstoque;
+        private System.Windows.Forms.Button btAtualizar;
+        private System.Windows.Forms.Label lbQtdEstoque;
+        private System.Windows.Forms.DataGridView grvProduto;
+        private System.Windows.Forms.DataGridView grvVenda;
+        private System.Windows.Forms.Label lbProduto;
+        private System.Windows.Forms.Label lbVenda;
+    }
+}
+
---
+++ Estoque/Form1.cs
@@ -0,0 +1,140 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Estoque
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        int qtdEstoque = 0;
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            //INICIALIZANDO LISTAS
+            InicializaListas();
+            //INICIANDO VALOR ESTOQUE ANTES DE ATUALIZAR
+            lbQtdEstoque.Text = qtdEstoque.ToString();
+
+        }
+
+        public void InicializaListas()
+        {
+            List<Produto> produtos = preencheListaProduto();
+            List<Venda> vendas = preencheListaVenda();
+            List<Filial> filiais = preencheListaFilial();
+
+            grvProduto.DataSource = produtos;
+            grvVenda.DataSource = vendas;
+            qtdEstoque = produtos.Sum(x => (x.qtdeEstoque));
+        }
+
+        void atualizaEstoque()
+        {
+            foreach (DataGridViewRow grvRow in grvVenda.Rows)
+            {
+                RemoveProdutoVendido(grvRow.Cells["codigoProd"].Value.ToString(), grvRow.Cells["qtdVenda"].Value.ToString());
+            }
+        }
+
+        private void btAtualizar_Click(object sender, EventArgs e)
+        {
+            Thread thread = new Thread(atualizaEstoque);
+            thread.Start();
+            grvProduto.Refresh();
+            grvProduto.Update();
+            lbQtdEstoque.Text = qtdEstoque.ToString();
+            thread.Join();
+        }
+
+        private void RemoveProdutoVendido(string codidoProdutoVendido, string qtdVenda)
+        {
+            List<Produto> produtos = (List<Produto>)grvProduto.DataSource;
+
+            foreach (var prodt in produtos.Where(x => x.codigoProd == Convert.ToInt32(codidoProdutoVendido)))
+            {
+                prodt.qtdeEstoque = (prodt.qtdeEstoque - Convert.ToInt32(qtdVenda));
+            }
+            qtdEstoque = produtos.Sum(x => (x.qtdeEstoque));
+            grvProduto.DataSource = produtos;
+        }
+
+        private List<Filial> preencheListaFilial()
+        {
+            List<Filial> filiais = new List<Filial>();
+            for (int i = 1; i <= 10; i++)
+            {
+                filiais.Add(new Filial()
+                {
+                    codigoFil = i,
+                    nomeFil = "Filial " + i
+                });
+            }
+            return filiais;
+        }
+
+        private List<Venda> preencheListaVenda()
+        {
+            Random random = new Random();
+            List<Venda> vendas = new List<Venda>();
+            for (int i = 1; i <= 50; i++)
+            {
+                vendas.Add(new Venda()
+                {
+                    numVenda = i,
+                    qtdVenda = random.Next(1, 3),
+                    vlrVenda = random.Next(100, 1000).ToString(),
+                    codigoProd = random.Next(1, 10)
+                });
+            }
+            return vendas;
+        }
+
+        private List<Produto> preencheListaProduto()
+        {
+            Random random = new Random();
+
+            List<Produto> produtos = new List<Produto>();
+            for (int i = 1; i <= 10; i++)
+            {
+                produtos.Add(new Produto()
+                {
+                    codigoProd = i,
+                    nomeProd = "Produto " + i,
+                    qtdeEstoque = random.Next(50, 200)
+                });
+            }
+            return produtos;
+        }
+    }
+    public class Filial
+    {
+        public int codigoFil { get; set; }
+        public string nomeFil { get; set; }
+    }
+
+    public class Produto
+    {
+        public int codigoProd { get; set; }
+        public string nomeProd { get; set; }
+        public int qtdeEstoque { get; set; }
+    }
+
+    public class Venda
+    {
+        public int numVenda { get; set; }
+        public int codigoProd { get; set; }
+        public int qtdVenda { get; set; }
+        public string vlrVenda { get; set; }
+    }
+}
